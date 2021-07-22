524de40505f2a4622153eab2ef1283a848be19c6
yukimemi <yukimemi@gmail.com>
2021-07-23 01:11:44 +0900

    :zap: std change to 0.95

--- deps.ts
+++ deps.ts
@@ -4,7 +4,7 @@ export * as log from "https://deno.land/std/log/mod.ts";
 export * from "https://cdn.skypack.dev/lodash";
 export { Lock } from "https://deno.land/x/async/mod.ts";
 export { delay } from "https://deno.land/std/async/mod.ts";
-export { encodeToString } from "https://deno.land/x/std/encoding/hex.ts";
+export { encodeToString } from "https://deno.land/x/std@0.95.0/encoding/hex.ts";
 export { hmac } from "https://deno.land/x/crypto/hmac.ts";
 export { WebClient } from "https://deno.land/x/slack_web_api/mod.ts";
 
